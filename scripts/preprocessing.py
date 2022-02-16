1cd6ca4cba6b372c66584294227ba4427b37bcdc
Kaya Celebi <kayacelebi17@gmail.com>
2022-02-16 02:19:34 -0500

    script updates for foreign point vis

--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -3,7 +3,7 @@ import os
 import pandas as pd
 import numpy as np
 
-__all__ = ['getData','getLocationDF', 'getSortedKeys', 'getConnectedIndices', 'timeSub', 'rescaleDB', 'getDukeNodeData', 'OUT_OF_BOUNDS']
+__all__ = ['getData','getLocationDF', 'getSortedKeys', 'getConnectedIndices', 'timeSub', 'rescaleDB', 'getDukeNodeData', 'getForeignData', 'OUT_OF_BOUNDS']
 
 OUT_OF_BOUNDS = -300
 
@@ -19,51 +19,57 @@ OUT_OF_BOUNDS = -300
 		'date' : [list of dates]}
 '''
 def getData(directory):
-    data = {'id': [], 'location' : {'latitude': [], 'longitude': []}, 'cell_info': {'pci': [], 'ss': [], 'band': [], 'freq': []}, 'time_stamp': [], 'date' : [] }
+	data = {'id': [], 'location' : {'latitude': [], 'longitude': []}, 'cell_info': {'pci': [], 'ss': [], 'band': [], 'freq': []}, 'time_stamp': [], 'date' : [] }
 
-    for i, f in enumerate(os.listdir(directory)):
+	for i, f in enumerate(os.listdir(directory)):
+		new_data = json.load(open(directory + '/' + f))
 
-        new_data = json.load(open(directory + '/' + f))
+		signal_strengths = [x['ss'] for x in new_data['cell_info']]
 
-        data['id'] += [i]
+		if len(signal_strengths) == 0:
+			data['id'] += [i]
+			data['location']['longitude'] += [new_data['location']['longitude']]
+			data['location']['latitude'] += [new_data['location']['latitude']]
 
-        data['location']['longitude'] += [new_data['location']['longitude']]
-        data['location']['latitude'] += [new_data['location']['latitude']]
+			data['cell_info']['ss'] += [OUT_OF_BOUNDS]
+			data['cell_info']['pci'] += [None]
+			data['cell_info']['band'] += [None]
+			data['cell_info']['freq'] += [None]
+		else:
+			for j, v in enumerate(signal_strengths):
+				data['id'] += [i]
+				data['location']['longitude'] += [new_data['location']['longitude']]
+				data['location']['latitude'] += [new_data['location']['latitude']]
 
-        signal_strengths = [x['ss'] for x in new_data['cell_info']]
+				data['cell_info']['ss'] += [v]
+				data['cell_info']['pci'] += [new_data['cell_info'][j]['pci']]
+				data['cell_info']['band'] += [new_data['cell_info'][j]['band']]
+				data['cell_info']['freq'] += [new_data['cell_info'][j]['freq']]
 
-        data['cell_info']['ss'] += [[OUT_OF_BOUNDS] if len(signal_strengths) == 0 else signal_strengths]
-        data['cell_info']['pci'] += [ [x['pci'] for x in new_data['cell_info']] ]
-        data['cell_info']['band'] += [ [x['band'] for x in new_data['cell_info']] ]
-        data['cell_info']['freq'] += [ [x['freq'] for x in new_data['cell_info']] ]
+				data['time_stamp'] += [timeFormat(new_data['datetime']['time'])]
 
-        data['time_stamp'] += [timeFormat(new_data['datetime']['time'])]
+				data['date'] += [dateFormat(new_data['datetime']['date'])]
 
-        data['date'] += [dateFormat(new_data['datetime']['date'])]
+	return data
 
-    return data
-
-def getForeignData(directory):
-	duke_data = {'id': [], 'location' : {'latitude': [], 'longitude': []}, 'cell_info': {'ss': []}, 'time_stamp': [], 'date' : []}
+def getForeignData(data):
+	foreign_data = {'id': [], 'location' : {'latitude': [], 'longitude': []}, 'cell_info': {'ss': [], 'pci': [], 'band' : [], 'freq': []}, 'time_stamp': [], 'date' : []}
 
 	for i in range(len(data['id'])):
-		x = []
-		if 40 in data['cell_info']['pci'][i]:
-			x += [data['cell_info']['ss'][i][ data['cell_info']['pci'][i].index(40) ]]
-		if 20 in data['cell_info']['pci'][i]:
-			x += [data['cell_info']['ss'][i][ data['cell_info']['pci'][i].index(20) ]]
-
-		if len(x) > 0:
-			duke_data['cell_info']['ss'] += [np.mean(x)]
+		if 40 != data['cell_info']['pci'][i] and 20 != data['cell_info']['pci'][i]:
+			foreign_data['cell_info']['ss'] += [data['cell_info']['ss'][i]]
 		else:
-			duke_data['cell_info']['ss'] += [OUT_OF_BOUNDS]
+			foreign_data['cell_info']['ss'] += [OUT_OF_BOUNDS]
 
-	duke_data['id'] = data['id']
-	duke_data['location'] = data['location']
-	duke_data['time_stamp'] = data['time_stamp']
-	duke_data['date'] = data['date']
+	foreign_data['id'] = data['id']
+	foreign_data['location'] = data['location']
+	foreign_data['time_stamp'] = data['time_stamp']
+	foreign_data['date'] = data['date']
+	foreign_data['cell_info']['pci'] = data['cell_info']['pci']
+	foreign_data['cell_info']['band'] = data['cell_info']['band']
+	foreign_data['cell_info']['freq'] = data['cell_info']['freq']
 
-	return duke_data
+	return foreign_data
 
 
 '''
@@ -71,28 +77,21 @@ def getForeignData(directory):
 '''
 
 def getDukeNodeData(data):
-    duke_data = {'id': [], 'location' : {'latitude': [], 'longitude': []}, 'cell_info': {'ss': [], 'band': [], 'freq': []}, 'time_stamp': [], 'date' : []}
-    for i in range(len(data['id'])):
-        x = []
-        if 40 in data['cell_info']['pci'][i]:
-            x += [data['cell_info']['ss'][i][data['cell_info']['pci'][i].index(40)]]
-        if 20 in data['cell_info']['pci'][i]:
-            x += [data['cell_info']['ss'][i][data['cell_info']['pci'][i].index(20)]]
+	duke_data = {'id': [], 'location' : {'latitude': [], 'longitude': []}, 'cell_info': {'ss': [], 'band': [], 'freq': []}, 'time_stamp': [], 'date' : []}
+	for i in range(len(data['id'])):
+		if 40 == data['cell_info']['pci'][i] or 20 == data['cell_info']['pci'][i]:
+			duke_data['cell_info']['ss'] += [data['cell_info']['ss'][i]]
+		else:
+			duke_data['cell_info']['ss'] += [OUT_OF_BOUNDS]
 
-        if len(x) > 0:
-            duke_data['cell_info']['ss'] += [np.mean(x)]
-        else:
-            duke_data['cell_info']['ss'] += [OUT_OF_BOUNDS]
+	duke_data['id'] = data['id']
+	duke_data['location'] = data['location']
+	duke_data['time_stamp'] = data['time_stamp']
+	duke_data['date'] = data['date']
+	duke_data['cell_info']['band'] = data['cell_info']['band']
+	duke_data['cell_info']['freq'] = data['cell_info']['freq']
 
-    duke_data['id'] = data['id']
-    duke_data['location'] = data['location']
-    duke_data['time_stamp'] = data['time_stamp']
-    duke_data['date'] = data['date']
-    duke_data['cell_info']['band'] = data['cell_info']['band']
-    duke_data['cell_info']['freq'] = data['cell_info']['freq']
-    
-
-    return duke_data
+	return duke_data
 
 '''
 	Converts location element in data structure to pandas data frame to be
